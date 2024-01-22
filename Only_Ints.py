140e9a5d454054f8564139ea9f1a46979c025374
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-22 16:36:43 -0500

    commit with instructions

---
+++ Only_Ints.py
@@ -0,0 +1,5 @@
+#   Write a function named only_ints that takes two parameters. 
+#   Function should return True if both parameters are integers, and False otherwise.
+
+#E.g, calling only_ints(1,2) should return True, 
+#while calling only_ints("a", 1) should return False.
