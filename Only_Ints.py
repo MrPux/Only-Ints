8449610e6a8517f3cd3314b9d0aab5f0277b96c9
MrPuxe <anthondeleonsketch@gmail.com>
2024-01-22 19:16:57 -0500

    Finished function

--- Only_Ints.py
+++ Only_Ints.py
@@ -3,3 +3,9 @@
 
 #E.g, calling only_ints(1,2) should return True, 
 #while calling only_ints("a", 1) should return False.
+
+def only_ints(one, two):
+    return True if isinstance(one, int) and isinstance(two, int) else False
+
+print(only_ints(1, 3)) # True
+print(only_ints("a", 3)) # False
